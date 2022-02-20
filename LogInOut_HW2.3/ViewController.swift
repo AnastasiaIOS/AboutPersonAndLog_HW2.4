d2dc79056470614b02d4ec4ca9e74c81d254784f
Anastasia <pavlova.a.s@inbox.ru>
2022-02-20 22:00:29 +0300

    add new

--- LogInOut_HW2.3/ViewController.swift
+++ LogInOut_HW2.3/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
