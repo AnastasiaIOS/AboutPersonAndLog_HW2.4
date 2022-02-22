e91634c42fc8c1800b898894074161ff30d25ba5
Anastasia <pavlova.a.s@inbox.ru>
2022-02-22 20:49:11 +0300

    add new

--- LogInOut_HW2.3/LoginViewController.swift
+++ LogInOut_HW2.3/LoginViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
@@ -25,29 +25,43 @@ class ViewController: UIViewController {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
         welcomeVC.user = user
     }
-
-   
+    
+    
     @IBAction func loginButton() {
+        
+        if userNameTF.text != user || passwordTF.text != password {
+            //Alert(title: "", message: "", textField: )
+            let alert = UIAlertController(title: "Attention!", message: "User name or password is not correct", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default) { action in
+                // обработка нажатия кнопки
+            })
+            self.present(alert, animated: true, completion: nil)
+            
+            if passwordTF.text != password {
+                passwordTF.text = ""
+            }
+        }
+        
         
     }
     
     // Инф сообщение о логине
     @IBAction func showAlertAboutUsername() {
         
-        let alertUser = UIAlertController(title: "Attention!", message: "Username = user", preferredStyle: .alert)
+        _ = UIAlertController(title: "Attention!", message: "Username = user", preferredStyle: .alert)
     }
     
     //Инф сообщение о пароле
     @IBAction func showAlertAboutPassword() {
         
-        let alertPass = UIAlertController(title: "Attention!", message: "Password = 12345", preferredStyle: .alert)
+        _ = UIAlertController(title: "Attention!", message: "Password = 12345", preferredStyle: .alert)
     }
     
     
     
-    }
-    
-        
-    
+}
+
+
+
 
 
